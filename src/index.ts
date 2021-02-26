6bcb59ffb7c2114bb3af9f4af8960319fa495d3f
Michal <majklfly@gmail.com>
2021-02-26 21:36:11 +0100

    allowing cors

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,14 @@ import cors from "cors";
 
   //allowing all cors
   app.use(cors());
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
 
   // parsing cookie middleware followed
   app.use(cookieParser());
